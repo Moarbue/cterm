d6b9de7d63b521c697e404530b408bd93071802c
Moarbue <83272310+Moarbue@users.noreply.github.com>
2023-01-13 18:04:10 +0100

    Implement linear algebra structs and functions

---
+++ src/lineal.c
@@ -0,0 +1,159 @@
+#include "lineal.h"
+
+Vec2f vec2f(float x, float y)
+{
+    return (Vec2f) {.x = x, .y = y};
+}
+
+Vec2f vec2fs(float x)
+{
+    return (Vec2f) {.x = x, .y = x};
+}
+
+Vec2f vec2f_add(Vec2f a, Vec2f b)
+{
+    return (Vec2f) {.x = a.x + b.x, .y = a.y + b.y};
+}
+
+Vec2f vec2f_sub(Vec2f a, Vec2f b)
+{
+    return (Vec2f) {.x = a.x - b.x, .y = a.y - b.y};
+}
+
+Vec2f vec2f_mul(Vec2f a, Vec2f b)
+{
+    return (Vec2f) {.x = a.x * b.x, .y = a.y * b.y};
+}
+
+Vec2f vec2f_div(Vec2f a, Vec2f b)
+{
+    return (Vec2f) {.x = a.x / b.x, .y = a.y / b.y};
+}
+
+float vec2f_dot(Vec2f a, Vec2f b)
+{
+    return a.x * b.x + a.y * b.y;
+}
+
+//////////////////////////
+
+Vec2i vec2i(int x, int y)
+{
+    return (Vec2i) {.x = x, .y = y};
+}
+
+Vec2i vec2is(int x)
+{
+    return (Vec2i) {.x = x, .y = x};
+}
+
+Vec2i vec2i_add(Vec2i a, Vec2i b)
+{
+    return (Vec2i) {.x = a.x + b.x, .y = a.y + b.y};
+}
+
+Vec2i vec2i_sub(Vec2i a, Vec2i b)
+{
+    return (Vec2i) {.x = a.x - b.x, .y = a.y - b.y};
+}
+
+Vec2i vec2i_mul(Vec2i a, Vec2i b)
+{
+    return (Vec2i) {.x = a.x * b.x, .y = a.y * b.y};
+}
+
+Vec2i vec2i_div(Vec2i a, Vec2i b)
+{
+    return (Vec2i) {.x = a.x / b.x, .y = a.y / b.y};
+}
+
+int vec2i_dot(Vec2i a, Vec2i b)
+{
+    return a.x * b.x + a.y * b.y;
+}
+
+//////////////////////////
+
+Vec4f vec4f(float x, float y, float z, float w)
+{
+    return (Vec4f) {.x = x, .y = y, .z = z, .w = w};
+}
+
+Vec4f vec4fs(float x)
+{
+    return (Vec4f) {.x = x, .y = x, .z = x, .w = x};
+}
+
+Vec4f vec4f_add(Vec4f a, Vec4f b)
+{
+    return (Vec4f) {.x = a.x + b.x, .y = a.y + b.y, .z = a.z + b.z, .w = a.w + b.w};
+}
+
+Vec4f vec4f_sub(Vec4f a, Vec4f b)
+{
+    return (Vec4f) {.x = a.x - b.x, .y = a.y - b.y, .z = a.z - b.z, .w = a.w - b.w};
+}
+
+Vec4f vec4f_mul(Vec4f a, Vec4f b)
+{
+    return (Vec4f) {.x = a.x * b.x, .y = a.y * b.y, .z = a.z * b.z, .w = a.w * b.w};
+}
+
+Vec4f vec4f_div(Vec4f a, Vec4f b)
+{
+    return (Vec4f) {.x = a.x / b.x, .y = a.y / b.y, .z = a.z / b.z, .w = a.w / b.w};
+}
+
+float vec4f_dot(Vec4f a, Vec4f b)
+{
+    return a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w;
+}
+
+//////////////////////////
+
+Vec4i vec4i(int x, int y, int z, int w)
+{
+    return (Vec4i) {.x = x, .y = y, .z = z, .w = w};
+}
+
+Vec4i vec4is(int x)
+{
+    return (Vec4i) {.x = x, .y = x, .z = x, .w = x};
+}
+
+Vec4i vec4i_add(Vec4i a, Vec4i b)
+{
+    return (Vec4i) {.x = a.x + b.x, .y = a.y + b.y, .z = a.z + b.z, .w = a.w + b.w};
+}
+
+Vec4i vec4i_sub(Vec4i a, Vec4i b)
+{
+    return (Vec4i) {.x = a.x - b.x, .y = a.y - b.y, .z = a.z - b.z, .w = a.w - b.w};
+}
+
+Vec4i vec4i_mul(Vec4i a, Vec4i b)
+{
+    return (Vec4i) {.x = a.x * b.x, .y = a.y * b.y, .z = a.z * b.z, .w = a.w * b.w};
+}
+
+Vec4i vec4i_div(Vec4i a, Vec4i b)
+{
+    return (Vec4i) {.x = a.x / b.x, .y = a.y / b.y, .z = a.z / b.z, .w = a.w / b.w};
+}
+
+int vec4i_dot(Vec4i a, Vec4i b)
+{
+    return a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w;
+}
+
+//////////////////////////
+
+float lerp(float a, float b, float t)
+{
+    return a + (b - a) * t;
+}
+
+float clampf(float v, float low, float high)
+{
+    return v < low ? low : (v > high ? high : v);
+}
---
+++ src/lineal.h
@@ -0,0 +1,64 @@
+#ifndef _CTERM_LINEAL_H_
+#define _CTERM_LINEAL_H_
+
+typedef struct {
+    float x;
+    float y;
+} Vec2f;
+
+Vec2f vec2f(float x, float y);
+Vec2f vec2fs(float x);
+Vec2f vec2f_add(Vec2f a, Vec2f b);
+Vec2f vec2f_sub(Vec2f a, Vec2f b);
+Vec2f vec2f_mul(Vec2f a, Vec2f b);
+Vec2f vec2f_div(Vec2f a, Vec2f b);
+float vec2f_dot(Vec2f a, Vec2f b);
+
+typedef struct {
+    int x;
+    int y;
+} Vec2i;
+
+Vec2i vec2i(int x, int y);
+Vec2i vec2is(int x);
+Vec2i vec2i_add(Vec2i a, Vec2i b);
+Vec2i vec2i_sub(Vec2i a, Vec2i b);
+Vec2i vec2i_mul(Vec2i a, Vec2i b);
+Vec2i vec2i_div(Vec2i a, Vec2i b);
+int   vec2i_dot(Vec2i a, Vec2i b);
+
+typedef struct {
+    float x;
+    float y;
+    float z;
+    float w;
+} Vec4f;
+
+Vec4f vec4f(float x, float y, float z, float w);
+Vec4f vec4fs(float x);
+Vec4f vec4f_add(Vec4f a, Vec4f b);
+Vec4f vec4f_sub(Vec4f a, Vec4f b);
+Vec4f vec4f_mul(Vec4f a, Vec4f b);
+Vec4f vec4f_div(Vec4f a, Vec4f b);
+float vec4f_dot(Vec4f a, Vec4f b);
+
+typedef struct {
+    int x;
+    int y;
+    int z;
+    int w;
+} Vec4i;
+
+Vec4i vec4i(int x, int y, int z, int w);
+Vec4i vec4is(int x);
+Vec4i vec4i_add(Vec4i a, Vec4i b);
+Vec4i vec4i_sub(Vec4i a, Vec4i b);
+Vec4i vec4i_mul(Vec4i a, Vec4i b);
+Vec4i vec4i_div(Vec4i a, Vec4i b);
+int   vec4i_dot(Vec4i a, Vec4i b);
+
+
+float lerp(float a, float b, float t);
+float clampf(float v, float low, float high);
+
+#endif // _CTERM_LINEAL_H_
