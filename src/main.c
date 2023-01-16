b5f226801adec0354026941207f24cdfb602daed
Moarbue <83272310+Moarbue@users.noreply.github.com>
2023-01-16 18:29:17 +0100

    Add button example

--- src/main.c
+++ src/main.c
@@ -7,14 +7,19 @@
 #include "util.h"
 #include "lineal.h"
 #include "renderer.h"
+#include "button.h"
 
 GLFWwindow *window;
 int ww, wh;
 
 Renderer r = {0};
+    
+Button btn;
 
 void cleanup(void);
 void glfw_process_keyboard_input(GLFWwindow *window);
+void glfw_mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+void btn0_on_click(int btn);
 
 int main()
 {
@@ -23,14 +28,16 @@ int main()
     set_up_glfw();
     // Create and configure window
     window = create_window();
+    glfwSetMouseButtonCallback(window,glfw_mouse_button_callback);
     // Load OpenGL functions
     load_opengl_functions();
 
     glClearColor(0.7f, 0.9f, 0.1f, 1.0f);
 
     renderer_init(&r, "shaders/cterm.vert", "shaders/color.frag", "shaders/texture.frag");
-    texture_new("resources/prister.jpg");
+    texture_new("resources/red_button.jpg");
 
+    button(&btn, -.08, -.08, .2, .2, 0.f, 0.f, 1.f, 1.f, btn0_on_click);
 
     // main loop
     while (glfwWindowShouldClose(window) == GLFW_FALSE) {
@@ -39,24 +46,7 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
         
         renderer_set_shader(&r, SHADER_TEXTURE);
-        renderer_texture_rect(&r, -.5f, -.5f, 1.f, 1.f, 0.f, 0.f, 1.f, 1.f);
-
-        renderer_set_shader(&r, SHADER_COLOR);
-        renderer_solid_rect(&r, 0.3f, 0.35f, 0.21f, 0.3f, vec4fs(1.f));
-        Vec4f red   = vec4f(1.f, 0.f, 0.f, 1.f);
-        Vec4f black = vec4f(0.f, 0.f, 0.f, 1.f);
-        renderer_set_shader(&r, SHADER_COLOR);
-        renderer_line(&r, vec2f(-1.f, 1.f),  vec2f(1.f, -1.f), black, 0.05);
-        renderer_line(&r, vec2f(-1.f, -1.f), vec2f(1.f, 1.f),  black, 0.05);
-        renderer_line(&r, vec2f(0.f, .9f),   vec2f(0.f, -.9f), black, 0.05);
-        renderer_line(&r, vec2f(-1.f, 0.f),  vec2f(1.f, 0.f),  black, 0.05);
-        renderer_line(&r, vec2f(0.f, .9f),   vec2f(0.f, -.9f), red,   0.006);
-        renderer_line(&r, vec2f(-1.f, 0.f),  vec2f(1.f, 0.f),  red,   0.006);
-
-        renderer_set_shader(&r, SHADER_COLOR);
-        renderer_cubic_bezier(&r, vec2f(-.5f, -.5f), vec2f(-.45, .5f), vec2f(.45f, .5f), vec2f(.5f, -.5f), black, 0.01);
-        renderer_cubic_bezier(&r, vec2f(-.6f, -.14f), vec2f(-.45, .5f), vec2f(.45f, .5f), vec2f(.9f, -.46f), black, 0.01);
-        renderer_cubic_bezier(&r, vec2f(-.6f, .14f), vec2f(-.45, -.5f), vec2f(.45f, -.5f), vec2f(.9f, .46f), black, 0.01);
+        button_render(&btn, &r);
 
         r.time = glfwGetTime();
         glfwSwapBuffers(window);
@@ -75,5 +65,27 @@ void cleanup(void)
 void glfw_process_keyboard_input(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+void glfw_mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+    (void)mods;
+    double cx, cy;
+    glfwGetCursorPos(window, &cx, &cy);
+    glfwGetWindowSize(window, &ww, &wh);
+
+    cx = cx / (double)ww * 2 - 1;
+    cy = cy / (double)wh * 2 - 1;
+
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+        button_update(&btn, (float)cx, (float)cy, button);
+    }
+
+}
+
+void btn0_on_click(int btn)
+{
+    if (btn == GLFW_MOUSE_BUTTON_LEFT)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
